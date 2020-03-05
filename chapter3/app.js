5a5d238a07a2dfb0c9128ca373adb60d578adc97
respectable2007 <zhanghui8876@163.com>
2020-03-05 16:33:15 +0800

    chapter3

--- chapter3/app.js
+++ chapter3/app.js
@@ -19,6 +19,16 @@ if(module === require.main){
 //on/addListener绑定事件名及事件处理函数
 var http = require('http')
 var server = http.createServer()
+
+/* EventEmitter类有newListener和removeListener事件，
+当继承了EventEmitter类的对象绑定／解绑事件时会触发newListener／removeListener事件 */
+
+server.on('newListener', function() {
+  console.log('绑定事件了')
+})
+server.on('removeListener', function() {
+  console.log('解绑事件了')
+})
 server.on('request', function(req, res) {
   if (req.url !== '/favicon.ico') {
     console.log(req.url, 1)
@@ -48,15 +58,29 @@ server.on('request', function(req, res) {
   res.end()
 }) */
 
-//解绑某个事件类型或所有事件类型的事件处理函数，removeAllListeners('事件类型')或不填(解绑所有)
 
+//解绑某个事件类型或所有事件类型的事件处理函数，removeAllListeners('事件类型')或不填(解绑所有)
 server.removeAllListeners('request')
+
+//手工触发事件,object.emit
+server.on('customeEvent', function(a, b, c) {
+  console.log(a, b, c)
+})
+
+server.emit('customeEvent', '1', '2', '3')
+
 server.on('request', function(req, res) {
   if(req.url !== '/favicon.ico') {
     console.log('hello world')
   }
   res.end()
 })
+
+
+
+//获取某个对象的某个事件类型的事件处理函数
+var event = require('events')
+console.log(event.EventEmitter.listenerCount(server, 'customeEvent'))
 
 
 
